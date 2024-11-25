648bf1ec4e7d84fc4c4345b570dc7590db12c0ae
Codecyte <74274252+Codecyte@users.noreply.github.com>
2024-11-25 13:54:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function generateSchedule(Techs, OneToOnesGendered, OneToOnesRegular, jobStartOf
 
   
   for (let tech = 0; tech < Techs; tech++) {
-    if (tech == Techs - 1) {
+    if (Techs >= 5 && (firstBreak + (tech % breakHours) === (lastBreak + 1))) {
         schedule[((tech + breakStartOffset) % Techs)][firstBreak + (tech % breakHours)] = lastBreakString;
     } else {
         schedule[((tech + breakStartOffset) % Techs)][firstBreak + (tech % breakHours)] = breakString;
